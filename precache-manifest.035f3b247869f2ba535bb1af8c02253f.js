ea25f515bc5adfee0b3bdb12027167de58258304
Merriman-Sly <ikembah2020@gmail.com>
2020-10-05 14:38:54 +0100

    Updates

---
+++ precache-manifest.035f3b247869f2ba535bb1af8c02253f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d153e2f5b085daf7312942b23874aa08",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "610e4d72d6fab3717a63",
+    "url": "/Monster-rolodex/static/css/main.db93f41c.chunk.css"
+  },
+  {
+    "revision": "66408a0eadc0e4d68aac",
+    "url": "/Monster-rolodex/static/js/2.e1495d10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-rolodex/static/js/2.e1495d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610e4d72d6fab3717a63",
+    "url": "/Monster-rolodex/static/js/main.10889ada.chunk.js"
+  },
+  {
+    "revision": "4e35c9efa1de34dea7ee",
+    "url": "/Monster-rolodex/static/js/runtime-main.60a0aa0b.js"
+  }
+]);
